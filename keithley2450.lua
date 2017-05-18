811e8de49758522a9a2e0c3be9a92427872ed14d
gracecummings <cummingsge@vcu.edu>
2017-05-18 12:22:52 -0400

    Added IVRunnerList(vList) to the lua script

--- keithley2450.lua
+++ keithley2450.lua
@@ -1,3 +1,67 @@
+function IVRunnerList(vList)
+   -- Given a list of voltages (vList), step through each voltage
+   display.clear()
+   display.clear()
+   display.settext(display.TEXT1, "Running I-V Scan")
+
+   --disable autorange as we'll handle it 
+   smu.measure.autorange = smu.OFF
+   smu.measure.range = 10e-9 --10nA is the minimum of the 2450
+   
+   defbuffer1.clear()   
+   defbuffer1.appendmode = 1
+--   defbuffer1.collectsourcevalues = 1
+
+   
+   smu.source.level = vList[1]
+   i = smu.measure.read()
+   print(string.format('%s', i))
+   i = smu.measure.read()
+   print(string.format('%s', i))
+   i = smu.measure.read()
+   print(string.format('%s', i))
+   i = smu.measure.read()
+   print(string.format('%s', i))
+   defbuffer1.clear()   
+
+   element = 1
+   while vList[element] do
+      smu.source.level = vList[element]
+      i = smu.measure.read()
+
+      rangeCheck = math.abs(i) > 0.8*smu.measure.range
+      
+      if rangeCheck then
+      	 print ('Close to range limit, increasing to next level')
+      	 smu.measure.range = smu.measure.range*10
+      	 print(string.format("New Range: %.3g nA", smu.measure.range))
+      end
+
+            voltage = string.format("%.5g V", smu.source.level)
+      current = string.format("%.3g nA", 1e9*i)
+
+      display.settext(display.TEXT1, string.format("%s", current))
+
+      display.settext(display.TEXT2, string.format("%s", voltage))
+      print(string.format('%s, %s', voltage, current))
+      element = element + 1
+
+   end
+
+   print('Done')
+   smu.source.level = 0
+   smu.source.output = smu.OFF
+   display.clear()
+   display.settext(display.TEXT1, "Done")
+   print('Done')
+
+end
+
+
+
+
+
+
 function IVRunner(start, stop, step)
 --   errorqueue.clear()
    display.clear()
@@ -15,13 +79,13 @@ function IVRunner(start, stop, step)
    
    smu.source.level = start
    i = smu.measure.read()
-   print(string.format('%s', current))
+   print(string.format('%s', i))
    i = smu.measure.read()
-   print(string.format('%s', current))
+   print(string.format('%s', i))
    i = smu.measure.read()
-   print(string.format('%s', current))
+   print(string.format('%s', i))
    i = smu.measure.read()
-   print(string.format('%s', current))
+   print(string.format('%s', i))
    defbuffer1.clear()   
    for vstep = start, stop, step do
       smu.source.level = vstep
